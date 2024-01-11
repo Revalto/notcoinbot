2baa6ccfbe821b45bc8470b72acbc1c4aad913af
Revalto <samuel.nestro@mail.ru>
2024-01-11 15:34:28 +0300

    release

---
+++ index.js
@@ -0,0 +1,33 @@
+const NotCoin = require('node-notcoinapi');
+const notCoin = new NotCoin(`СЮДА ССЫЛКУ`);
+
+(async () => {
+    await notCoin.event();
+    await notCoin.auth();
+
+    setInterval(async () => {
+        try {
+            let profile = notCoin.getProfile();
+
+            if(profile.availableCoins < 100) {
+                return;
+            }
+
+            let clickCount = 1;
+
+            if(profile.availableCoins > 1790) {
+                clickCount = 1590;
+            } else {
+                clickCount = notCoin.rand(1, profile.availableCoins - 100);
+            }
+
+            await notCoin.click(clickCount);
+
+            profile = notCoin.getProfile();
+
+            console.log(`Click count: ${profile.availableCoins} | Balance: ${profile.balanceCoins}`);
+        } catch (e) {
+            console.log(e.message);
+        }
+    }, 15000);
+})();
